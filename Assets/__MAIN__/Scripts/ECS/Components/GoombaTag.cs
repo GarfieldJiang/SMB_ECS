7c0e8e00f3acdff418dd65307587cdc88a77e448
jehovah0121 <jehovah0121@gmail.com>
2020-04-28 22:17:22 +0800

    Remove unused component data struct:

--- Assets/__MAIN__/Scripts/ECS/Components/GoombaTag.cs
+++
@@ -1,9 +0,0 @@
-using Unity.Entities;
-
-/// <summary>
-/// Component data for enemies that the player can stomp.
-/// </summary>
-[GenerateAuthoringComponent]
-public struct GoombaTag : IComponentData
-{
-}
